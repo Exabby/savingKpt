ea8d7ac4e2cb63ac00c997697df5a60657743ac2
Exabby <teerapatfluck46@gmail.com>
2021-11-28 21:53:19 +0700

    Update signUp.dart

--- lib/screen/signUp.dart
+++ lib/screen/signUp.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'memberlogin.dart';
 
-class SingUp extends StatelessWidget {
+class SingUp extends StatefulWidget {
   const SingUp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("ผู้ใช้"),
+          title: Text("สมัครเข้าใช้งาน"),
         ),
         body: Column());
   }
