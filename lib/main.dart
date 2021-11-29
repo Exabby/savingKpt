ad700f2c5844af2d29d1d8f4a7d83ccffb9a0c7f
Exabby <teerapatfluck46@gmail.com>
2021-11-29 10:58:09 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,14 @@
-// ignore_for_file: prefer_const_constructors, unused_import
-
-import 'dart:io';
-import 'dart:typed_data';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:kptsave/screen/memberlogin.dart';
+import 'screen/home.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  ByteData data =
-      await PlatformAssetBundle().load('assets/ca/lets-encrypt-r3.pem');
-  SecurityContext.defaultContext
-      .setTrustedCertificatesBytes(data.buffer.asUint8List());
+void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -27,7 +17,9 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.indigo,
       ),
       home: MemberLogin(),
+      routes: {
+        Homekpts.routeName: (context) => const Homekpts(),
+      },
     );
-    // ignore: dead_code
   }
 }
