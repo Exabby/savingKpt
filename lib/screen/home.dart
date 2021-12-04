6bef29d4639de75816d8e8c8b6a242177787a223
Exabby <teerapatfluck46@gmail.com>
2021-12-04 14:54:56 +0700

    เพิ้มรูปแบบกดได้หน้า Home

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -39,42 +39,142 @@ class Homekpts extends StatelessWidget {
         ),
         Padding(
           padding: const EdgeInsets.only(top: 10, left: 10),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                width: 100,
-                height: 100,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                      image: NetworkImage(
-                          'https://save.kpt.ac.th/' + args.profilePic),
-                      fit: BoxFit.fill),
-                ),
+          child: Column(
+            children: <Widget>[
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                          image: NetworkImage(
+                              'https://save.kpt.ac.th/' + args.profilePic),
+                          fit: BoxFit.fill),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8.0, top: 20),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          args.title + args.name + " " + args.lname,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 13.0),
+                          child: Text(
+                            args.id,
+                            overflow: TextOverflow.ellipsis,
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 18),
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
               ),
               Padding(
-                padding: const EdgeInsets.only(left: 8.0, top: 20),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Text(
-                      args.title + args.name + " " + args.lname,
-                      overflow: TextOverflow.ellipsis,
-                      style: const TextStyle(
-                          fontWeight: FontWeight.bold, fontSize: 20),
+                padding: const EdgeInsets.only(top: 50, left: 10),
+                child: Row(children: [
+                  GestureDetector(
+                    onTap: null,
+                    child: Image.asset(
+                      'assets/images/ข้อมูลออมทรัพย์.png',
+                      fit: BoxFit.cover,
                     ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 13.0),
-                      child: Text(
-                        args.id,
-                        overflow: TextOverflow.ellipsis,
-                        style: const TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 40),
+                    child: GestureDetector(
+                      onTap: null,
+                      child: Image.asset(
+                        'assets/images/ข้อมูลออมทรัพย์.png',
+                        fit: BoxFit.cover,
                       ),
-                    )
-                  ],
-                ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 40),
+                    child: GestureDetector(
+                      onTap: null,
+                      child: Image.asset(
+                        'assets/images/ข้อมูลออมทรัพย์.png',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ]),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 50, left: 10),
+                child: Row(children: [
+                  GestureDetector(
+                    onTap: null,
+                    child: Image.asset(
+                      'assets/images/ข้อมูลออมทรัพย์.png',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 40),
+                    child: GestureDetector(
+                      onTap: null,
+                      child: Image.asset(
+                        'assets/images/ข้อมูลออมทรัพย์.png',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 40),
+                    child: GestureDetector(
+                      onTap: null,
+                      child: Image.asset(
+                        'assets/images/ข้อมูลออมทรัพย์.png',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ]),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 50, left: 10),
+                child: Row(children: [
+                  GestureDetector(
+                    onTap: null,
+                    child: Image.asset(
+                      'assets/images/ข้อมูลออมทรัพย์.png',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 40),
+                    child: GestureDetector(
+                      onTap: null,
+                      child: Image.asset(
+                        'assets/images/ข้อมูลออมทรัพย์.png',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 40),
+                    child: GestureDetector(
+                      onTap: null,
+                      child: Image.asset(
+                        'assets/images/ข้อมูลออมทรัพย์.png',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ]),
               )
             ],
           ),
